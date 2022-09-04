d9e883294e4c6db0ea2fe0d165b9e7b0cdc438af
char5742 <f.shuuma@gmail.com>
2022-09-04 16:57:55 +0900

    refactor: socket.io バージョンアップ

--- chatapp/sockets/index.js
+++ chatapp/sockets/index.js
@@ -1,18 +1,15 @@
-'use strict';
-
+"use strict";
+const { Server } = require("socket.io");
 module.exports = function (server) {
-
-    const socketIo = require('socket.io')(server, { wsEngine: 'ws' });
-    const io = socketIo.listen(server);
-
-    io.sockets.on('connection', function (socket) {
+    const io = new Server(server);
+    io.on("connection", async function (socket) {
         // 投稿モジュールの呼出
-        require('./publish')(socket, io);
+        require("./publish")(socket, io);
 
         // 入室モジュールの呼出
-        require('./enter')(socket);
+        require("./enter")(socket, io);
 
         // 退室モジュールの呼出
-        require('./exit')(socket);
+        require("./exit")(socket);
     });
 };
