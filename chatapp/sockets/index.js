89f92ec5f264233476447675a1098bf0640ca05a
char5742 <f.shuuma@gmail.com>
2022-09-04 17:07:45 +0900

    refactor: userNameをsocketから取得できるように

--- chatapp/sockets/index.js
+++ chatapp/sockets/index.js
@@ -3,6 +3,7 @@ const { Server } = require("socket.io");
 module.exports = function (server) {
     const io = new Server(server);
     io.on("connection", async function (socket) {
+        socket.data.userName = socket.handshake.query.name;
         // 投稿モジュールの呼出
         require("./publish")(socket, io);
 
