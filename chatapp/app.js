00c29a2055e7f595cf7d15277e5520e562257a63
char5742 <f.shuuma@gmail.com>
2022-09-10 23:07:02 +0900

    fix: 不要なhbsの削除

--- chatapp/app.js
+++ chatapp/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+app.set("view engine", "html");
+app.engine("html", hbs.__express);
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
