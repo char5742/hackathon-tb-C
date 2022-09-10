27cf1ae44300bad131aad2188f2051872e4995aa
Char☆ <50830649+char5742@users.noreply.github.com>
2022-09-10 23:02:57 +0900

    Feature/room design (#16)
    * refactor: index, room hbsの微修正
    
    * 新規登録画面 (#14)
    
    * refactor: layout.hbsを適切な値に
    
    * refactor: hbs->html
    
    * refactor: roomページをデザインどおりに
    
    Co-authored-by: もち <101976431+Mochi-frontend@users.noreply.github.com>

--- chatapp/app.js
+++ chatapp/app.js
@@ -6,6 +6,7 @@ const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const hbs = require("hbs");
 
 const routes = require("./routes/index");
 
