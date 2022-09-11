ec239018db64f966ef0cfb524d54fb4ca4c64638
Nanaumit <nanatomo428@gmail.com>
2022-09-12 02:58:48 +0900

    入退室時刻の表示，入退室メッセージの修正

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -8,6 +8,22 @@ socket.emit("enterMyselfEvent", userName);
 
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on("enterOtherEvent", function (name) {
-    $("#thread").prepend("<div class='d-inline-flex rounded-pill p-2 bg-secondary opacity-75 justify-content-center text-white'>" + name + "さんが入室しました。</div>");
+    // 入室時刻のフォーマット変換
+    const date = new Date();
+    const hour = ("00" + date.getHours()).slice(-2);
+    const minute = ("00" + date.getMinutes()).slice(-2);
+    const date_f = `${hour}:${minute}`;
+    
+    $("#thread").prepend(
+        `<div class='d-inline-flex flex-column rounded-pill p-2 pt-1 mb-3 bg-secondary opacity-50 justify-content-center text-center text-white'>
+            <p class="m-0" style="font-size: 0.5em">${date_f}</p>
+            <p class="m-0">${name}さんが入室しました。</p>
+        </div>`);
+    
+    // 最新の投稿までスクロールする
+    $("#thread_space").animate(
+        {scrollTop: $("#thread_space")[0].scrollHeight - $("#thread_space").height()}
+    );
+
     socket.emit("getAllUsernameEvent");
 });
--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -11,6 +11,21 @@ function exit() {
 }
 
 // サーバから受信した退室メッセージを画面上に表示する
-socket.on('exitOtherEvent', function (data) {
-    $('#thread').prepend('<div class="d-inline-flex rounded-pill p-2 bg-secondary opacity-75 justify-content-center text-white">' + data + '</div>');
+socket.on('exitOtherEvent', function (name) {
+    // 退室時刻sendDateのフォーマット変換
+    const date = new Date();
+    const hour = ("00" + date.getHours()).slice(-2);
+    const minute = ("00" + date.getMinutes()).slice(-2);
+    const date_f = `${hour}:${minute}`;
+    
+    $("#thread").prepend(
+        `<div class='d-inline-flex flex-column rounded-pill p-2 pt-1 mb-3 bg-secondary opacity-50 justify-content-center text-center text-white'>
+            <p class="m-0" style="font-size: 0.5em">${date_f}</p>
+            <p class="m-0">${name}さんが退室しました。</p>
+        </div>`);
+    
+    // 最新の投稿までスクロールする
+    $("#thread_space").animate(
+        {scrollTop: $("#thread_space")[0].scrollHeight - $("#thread_space").height()}
+    );
 });
